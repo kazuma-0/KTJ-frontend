c80fddb9dfddae1dcdb4bf61fdd8412804d0044f
kazuma-0 <as8644459@gmail.com>
2022-09-11 12:54:43 +0530

    create supabase client

---
+++ src/supabase/client.js
@@ -0,0 +1,4 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { SUPABASE_API_KEY, SUPABASE_URL } from "../config";
+const client = new SupabaseClient(SUPABASE_URL, SUPABASE_API_KEY, {})
+export default client;

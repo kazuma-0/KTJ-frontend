efab0cb622be6012bb9829df6915b072f58cd2f1
kazuma-0 <as8644459@gmail.com>
2022-09-11 12:52:39 +0530

    add daysjs and provided supabase

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/tailwind.css'
+import './index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import {Provider} from "react-redux";
 import store from "./store/store";
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import SupabaseContext from './context/SupabaseContext'
+import client from './supabase/client'
+dayjs.extend(localizedFormat)
+dayjs.extend(relativeTime)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <React.StrictMode>
     <ChakraProvider>
-      {/* hello */}
-      <Provider store={store}>
-        <App/>
-      </Provider>
+      <SupabaseContext.Provider value={client}>
+      <App/>
+      </SupabaseContext.Provider>
     </ChakraProvider>
     </React.StrictMode>
   </>
